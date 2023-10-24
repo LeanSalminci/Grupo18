7c5cb581a1327bba4c188a29cbbf0f8e4b41c3f2
LeanSalminci <leandro.salminci@gmail.com>
2023-10-24 19:41:30 -0300

    nuevo cambio

---
+++ script.js
@@ -0,0 +1,11 @@
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
