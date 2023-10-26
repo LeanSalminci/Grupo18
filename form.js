77c1e64b3e6069cce1e9533939249f6b87103755
LeanSalminci <leandro.salminci@gmail.com>
2023-10-26 15:08:49 -0300

    cambio final

---
+++ form.js
@@ -0,0 +1,2 @@
+const userNameField = document.querySelector("[name=username]");
+const emailField = document.querySelector("[name=email]");
--- js/script.js
+++ js/script.js
@@ -9,3 +9,11 @@ function mostrarOcultarMenu(){
         menuVisible = true;
     }
 }
+
+
+
+
+
+
+
+
