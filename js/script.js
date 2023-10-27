e05ff046dddc63b7c0ebea66450df98cdf901276
LeanSalminci <leandro.salminci@gmail.com>
2023-10-27 11:35:08 -0300

    nuevo cambio final

--- js/script.js
+++ js/script.js
@@ -9,11 +9,3 @@ function mostrarOcultarMenu(){
         menuVisible = true;
     }
 }
-const formulario = document.getElementById('form-box')
-const inputs = document.querySelectorAll('#formulario input')
-const expresiones = {
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	telefono: /^\d{7,14}$/, // 7 a 14 numeros.
-    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	
-     }
